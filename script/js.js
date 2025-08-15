f7a0fc017dbbd9a923fdfeae6334f28fcf141452
gr-X-chemistry-website <akpavanaa2018@gmail.com>
2025-08-15 21:50:41 +0530

    Update js.js

--- script/js.js
+++ script/js.js
@@ -5,3 +5,21 @@ function toggleMobileMenu(menu) {
 function openHome(){
     window.open("./../index.html");
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const elementsToAnimate = document.querySelectorAll('.anim');
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('is-visible');
+          observer.unobserve(entry.target);
+        }
+      });
+    }, {
+      threshold: 0.5
+    });
+
+    elementsToAnimate.forEach(element => {
+      observer.observe(element);
+    });
+  });
